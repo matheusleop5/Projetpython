1d58ee3a203d2e5b7b5a25ccb5c552a2065fc330
matheusleop5 <matheusleop11@gmail.com>
2025-03-28 14:08:17 -0300

    atualização

--- calculadora_inteiros.py
+++ calculadora_inteiros.py
@@ -1,4 +1,12 @@
 print("Bem vindo a calculadora virtual")
- num1 = input("Digite o primeiro numero: ")
- num2 = input("Digite o segundo valor")
- print("O resultado da soma foi = "num1 + num2)
+num1 = int(input("Digite o primeiro valor: "))
+num2 = int(input("Digite o segundo valor: "))
+soma = num1 + num2
+subtracao = num1 - num2
+divisao = num1 / num2
+multiplicacao = num1 * num2
+
+print(f"O resultado da soma foi: {soma}")
+print(f"O resultado da subtração foi: {subtracao}")
+print(f"O resultado da divisão foi: {divisao}")
+print(f"O resultado da multiplicação foi: {multiplicacao}")
