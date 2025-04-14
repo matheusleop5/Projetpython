458ace1334c65c90f392d8e3bf738a15794e92e2
matheusleop5 <matheusleop11@gmail.com>
2025-04-14 16:35:33 -0300

    Utilizando atividades

---
+++ exercicios/lista.py
@@ -0,0 +1,18 @@
+lista = [12, 15.5, "textos"]
+print(type(lista))
+
+#inserindo novos elementos
+lista.append("teste de inserção")
+
+#mostrando a lista inteira
+print(lista)
+
+#mostrando elemento pelo indice
+print(lista[0:3])
+
+#Exibindo com loop
+for valor in lista:
+    print(valor)
+#removendo
+
+#tamanho da lista
