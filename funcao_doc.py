5c8d1c87605b71e12578b18947c0481de2a9adda
matheusleop5 <matheusleop11@gmail.com>
2025-04-23 16:33:28 -0300

    Atualização de estudos

---
+++ funcao_doc.py
@@ -0,0 +1,8 @@
+def somar(a:float, b:float):
+    """Função que realiza a soma entre dois valores e retorna um resultado.
+    Dois argumentos, a e b, são obrigatórios e são do tipo float"""
+    return a + b
+
+
+print(somar.__doc__)
+print(help(somar))
---
+++ gravando_json.py
@@ -0,0 +1,9 @@
+import json
+dicionario = {
+    "nome":"Python",
+    "missão":"Ser incrível!"
+}
+
+conteudo = json.dumps(dicionario, ensure_ascii=False, indent=4)
+with open("arquivo.json", mode="w", encoding="UTF-8") as arquivo:
+    arquivo.write(conteudo)
